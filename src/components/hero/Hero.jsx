53c50bd14cce621d6b3f60c9000093c5506ec577
Christine Contreras <christineec23@yahoo.com>
2022-04-13 15:07:16 -0500

    hero finished

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,11 +3,11 @@ import './hero.css'
 import arrow from '../../images/icons/icon-arrow-down.svg'
 const Hero = () => {
   return (
-    <section className='hero h-[90vh] max-h-[650px] flex justify-center items-center flex-col gap-y-20'>
+    <section className='hero h-[90vh] max-h-[750px] flex justify-center items-center flex-col gap-y-20'>
       <h1 className='font-serif text-4xl sm:text-5xl tracking-widest uppercase text-white text-center'>
         We Are Creatives
       </h1>
-      <img src={arrow} alt='view more about us' />
+      <img src={arrow} alt='view more about us' className='lg:pb-20' />
     </section>
   )
 }
